09808c4d5afd343b98fe7abffa34427d5870ae7a
Terrence Huang <terrencehuang2000@gmail.com>
2021-10-21 20:10:36 +0800

    Add NavItemsToggler and NavItem components

---
+++ src/components/NavItemsToggler.jsx
@@ -0,0 +1,17 @@
+function NavItemsToggler() {
+  return (
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  );
+}
+
+export default NavItemsToggler;
