96418cb7f7687cafca5ef24ea8b4a0eba2e0301f
Terrence Huang <terrencehuang2000@gmail.com>
2021-10-21 23:10:32 +0800

    Modify the TODO comment

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Home from "./components/Home";
 import LanguageChooser from "./components/LanguageChooser";
 import Navbar from "./components/Navbar";
 
-// TODO: Put to config, like homepage property in package.json
+// TODO: This is for github page, find a way to put to config in the future, homepage property in package.json also
 const routeBasename = "/react-playground";
 
 function App() {
